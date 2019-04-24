a8b9bae9de457d7323da083440de2f7591cac02c
hades <nafeeshhaider786.nh@gmail.com>
2019-04-24 17:05:22 +0530

    mysite app,first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e4p9@mh_s%&h89_m)7l11)#1^lbfh+@#h)-*i4y)2m65gyx+v@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
